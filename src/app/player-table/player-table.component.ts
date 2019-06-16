8c8ce090ed831253f8132cbb14a423e34316cb73
juaneins <ballesterosjg@gmail.com>
2019-06-16 08:44:34 -0500

    Implementando player table

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../interfaces/Player';
 import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-table',
@@ -9,14 +11,11 @@ import { PlayerService } from '../services/player.service';
   styleUrls: ['./player-table.component.scss']
 })
 export class PlayerTableComponent implements OnInit {
-
   public players$: Observable<Player[]>;
   public selectedPlayer: Player;
   public showModal = false;
 
-  constructor(private playerService: PlayerService) { 
-    
-  }
+  constructor(private playerService: PlayerService, private teamService: TeamService) {}
 
   ngOnInit() {
     this.players$ = this.playerService.getPlayers();
@@ -26,9 +25,34 @@ export class PlayerTableComponent implements OnInit {
     this.showModal = true;
     this.selectedPlayer = null;
     setTimeout(() => {
-      window.location.replace('#open');
+      window.location.replace('#open-modal');
     }, 0);
-
   }
 
+  editPlayer(player: Player) {
+    this.selectedPlayer = { ...player };
+    this.showModal = true;
+    setTimeout(() => {
+      window.location.replace('#open-modal');
+    });
+  }
+
+  deletePlayer(player: Player) {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        const moddifiedPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+        const formattedTeam = {
+          ...teams[0],
+          players: [...moddifiedPlayers]
+        };
+        this.playerService.deletePlayer(player.$key);
+        this.teamService.editTeam(formattedTeam);
+      });
+  }
+
+  closeDialog() {
+    this.showModal = false;
+  }
 }
